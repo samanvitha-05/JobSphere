7e87deb1bbaa3d2e8ec53f093482e0de246a92a4
Samanvitha <anvibhagavath05@gmail.com>
2026-07-09 21:36:39 +0530

    Added job pagination and sorting APIs

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -47,10 +47,25 @@ const createJob = async (req, res) => {
 // ======================
 const getAllJobs = async (req, res) => {
     try {
-        const jobs = await Job.find().populate("recruiter", "name email");
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 5;
+        
+        const skip = (page - 1) * limit;
+
+        const sortBy = req.query.sort || "-createdAt";
+
+        const jobs = await Job.find()
+        .populate("recruiter", "name email")
+        .sort(sortBy)
+        .skip(skip)
+        .limit(limit);
+        const totalJobs = await Job.countDocuments();
 
         res.status(200).json({
             success: true,
+            currentPage: page,
+            totalPages: Math.ceil(totalJobs / limit),
+            totalJobs,
             count: jobs.length,
             jobs
         });
