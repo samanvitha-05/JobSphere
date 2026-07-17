200a8b7b2d748c20838e21f816c30f31ec299e16
Samanvitha <anvibhagavath05@gmail.com>
2026-07-17 16:45:25 +0530

    Final project completed

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,5 +1,31 @@
+import MainLayout from "../layouts/MainLayout";
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-  return <h1>Not Found</h1>;
+
+    return (
+
+        <MainLayout>
+
+            <div className="text-center mt-5">
+
+                <h1>404</h1>
+
+                <h3>Page Not Found</h3>
+
+                <Link
+                    to="/"
+                    className="btn btn-primary mt-3"
+                >
+                    Go Home
+                </Link>
+
+            </div>
+
+        </MainLayout>
+
+    );
+
 };
 
 export default NotFound;
